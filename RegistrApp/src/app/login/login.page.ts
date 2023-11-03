9940ac81e50b64ecdc003adad79e85c4187f58db
NicoSotoL <NICO.SOTOL@DUOCUC.CL>
2023-11-03 07:01:01 -0300

    first

---
+++ RegistrApp/src/app/login/login.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  form = this.formBuilder.group({
+    email: ['', Validators.email, Validators.required],
+    password: ['', Validators.required]
+});
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
